2ab0ef9acdc691ca8ce2d6ff57884d3f1d64f96a
Veronika <veronika.marinkovic@exxeta.com>
2024-12-31 14:48:17 +0100

    Added missing fields check for login and registration Added positive fileds check for registration Added user already exists check for registration Fixed error with test for login when data is valid

--- src/test/java/com/mealplan/mealplan_app/cucumber/glue/UserLoginSteps.java
+++ src/test/java/com/mealplan/mealplan_app/cucumber/glue/UserLoginSteps.java
@@ -2,6 +2,7 @@ package com.mealplan.mealplan_app.cucumber.glue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mealplan.mealplan_app.user_service.dto.UserLoginDTO;
+import com.mealplan.mealplan_app.user_service.repository.UserRepository;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -23,6 +24,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ActiveProfiles("test")
 public class UserLoginSteps {
 
+    private static final String INSERT_USER_QUERY =
+            "INSERT INTO app_user (username, password, height, current_weight, target_weight, target_calories) " +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+
     private UserLoginDTO userLoginDTO;
     private ResponseEntity<String> response;
 
@@ -32,12 +37,17 @@ public class UserLoginSteps {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Before
     public void setupTestData() {
-        jdbcTemplate.execute("INSERT INTO app_user (username, password, height, current_weight, target_weight, target_calories) VALUES ('existingUser', 'password123', 175, 70, 65, 2000)");
-        jdbcTemplate.execute("INSERT INTO app_user (username, password, height, current_weight, target_weight, target_calories) VALUES ('existingUser2', 'password234', 180, 80, 75, 2200)");
+        userRepository.deleteAll();
+        insertUser("existingUser", "$2a$12$144tJ3p1AtyqZUITAz97UOIPEikSU6Zb2c5BW5NlGzddmoIK0tJLW",
+                175, 70, 65, 2000);
+        insertUser("existingUser2", "$2a$12$x5n62h9H7QeXTpL5/RlvBOeci/vt9m0tIXkXro9FPF2g6.2Zd6Onu",
+                180, 80, 75, 2200);
     }
-
 
     @Given("the user provides login credentials from file {string}")
     public void theUserProvidesLoginCredentialsFromFile(String fileName) throws IOException {
@@ -60,5 +70,9 @@ public class UserLoginSteps {
     @Then("the login system responds with {string}")
     public void theLoginSystemRespondsWith(String expectedMessage) {
         assertEquals(expectedMessage, response.getBody());
+    }
+
+    public void insertUser(String username, String password, int height, int currentWeight, int targetWeight, int targetCalories) {
+        jdbcTemplate.update(INSERT_USER_QUERY, username, password, height, currentWeight, targetWeight, targetCalories);
     }
 }
--- src/test/java/com/mealplan/mealplan_app/cucumber/glue/UserRegistrationSteps.java
+++ src/test/java/com/mealplan/mealplan_app/cucumber/glue/UserRegistrationSteps.java
@@ -1,13 +1,15 @@
 package com.mealplan.mealplan_app.cucumber.glue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mealplan.mealplan_app.user_service.dto.UserDTO;
+import com.mealplan.mealplan_app.user_service.dto.UserRegistrationDTO;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
@@ -18,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ActiveProfiles("test")
 public class UserRegistrationSteps {
 
-    private UserDTO userDTO;
+    private UserRegistrationDTO userDTO;
     private ResponseEntity<String> response;
 
     @LocalServerPort
@@ -28,7 +30,7 @@ public class UserRegistrationSteps {
     public void theUserProvidesRegistrationDetailsFromFile(String fileName) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         // Load the JSON file from the resources folder
-        userDTO = objectMapper.readValue(new File("src/test/resources/" + fileName), UserDTO.class);
+        userDTO = objectMapper.readValue(new File("src/test/resources/" + fileName), UserRegistrationDTO.class);
     }
 
     @When("the user sends a registration request")
@@ -37,8 +39,8 @@ public class UserRegistrationSteps {
         String url = "http://localhost:" + port + "/meal_plan/users/register";
         try {
             response = restTemplate.postForEntity(url, userDTO, String.class);
-        } catch (Exception e) {
-            response = ResponseEntity.badRequest().body(e.getMessage());
+        } catch (HttpClientErrorException | HttpServerErrorException e)  {
+            response = ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
         }
     }
 
